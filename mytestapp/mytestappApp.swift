e55780b7ed1f89e1c81b1478ec5f6b7c78e12c54
Jiwoo Han <blanketfys@gmail.com>
2023-12-26 23:10:02 +0900

    Initial Commit

---
+++ mytestapp/mytestappApp.swift
@@ -0,0 +1,17 @@
+//
+//  mytestappApp.swift
+//  mytestapp
+//
+//  Created by Jiwoo on 12/26/23.
+//
+
+import SwiftUI
+
+@main
+struct mytestappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
